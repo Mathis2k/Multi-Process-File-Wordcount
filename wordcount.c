9612a8454ae7d6feba5f1116a1e6e68762702bb5
tpp571 <tpp571@fox01.cs.utsa.edu>
2020-09-18 19:16:34 -0500

    wordcount.c near completion

--- wordcount.c
+++ wordcount.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 #define FORK_ERROR "ERROR: fork failed to execute."
 #define FILE_ERROR "ERROR: file does not exist."
+#define WAIT_ERROR "ERROR: failed to wait for child."
+
+int totalProcesses = 0;
+int totalUselessFiles = 0;
+int totalSuccessfulFiles = 0;
+int totalFiles = 0;
 
 int processFile(char* filename);
 int countNumberOfWords(char* string);
@@ -12,31 +20,66 @@ char* tokenizeString(char* string);
 
 int main(int argc, const char * argv[]) {
   
-    int i, returnedProcessFile;
+    int i, retWordCount, status, retVal;
+
+    printf("------------------ Running program ------------------\n");
+
     for(i=1; i<argc; i++){
-        int cpid = fork();
+        //int cpid = fork();
+        retWordCount = processFile(argv[i]);
+        
+        if(retWordCount == -1)
+            totalUselessFiles++;
+        else{
+            totalFiles++;
+            int cpid = fork();
+            if(cpid == 0){
+                //child process            
+                int pid = getpid();
 
-        if(cpid == 0){
-            //child process
-            returnedProcessFile = processFile(argv[i]);
-
-            if(returnedProcessFile == 0){ 
-                printf("Child process %d for %s: number of words is %d.\n", cpid, argv[i], wordCount);
-                return 0;
+                if(retWordCount != -1){ 
+                    printf("Child process %d for %s: number of words is %d.\n", pid, argv[i], retWordCount);
+                    return 0;
+                }
+                else{
+                    printf("Child process %d for %s: %s.\n", pid, argv[i], FILE_ERROR);
+                    exit(1);
+                }
             }
-            else{
-                printf("Child process %d for %s: %s.\n", cpid, argv[i], FILE_ERROR);
-                exit(1);
+            else if(cpid == -1){
+                //fork failed
+                fprintf(stderr, "%s\n", FORK_ERROR);
             }
-        }
-        else if(cpid == -1){
-            fprintf(stderr, "%s\n", FORK_ERROR);
         }           
    }    
    
 
     //TODO: wait on all child processes that spawned.
 
+    for(; ; ){
+
+        retVal = wait(&status);
+        if(retVal == -1 && (errno == EINTR)){
+            continue;
+        }    
+        else if(retVal == -1 && (errno == ECHILD)){
+            break;
+        }
+        else if(retVal == -1){
+            fprintf(stderr, "%s\n", WAIT_ERROR);
+            break;
+        }
+        else if(WIFEXITED(status) && !WEXITSTATUS(status)){
+            printf("Child %ld terminated normally.\n", (long)retVal);
+            totalSuccessfulFiles++;
+        }
+       
+        totalProcesses++; 
+    }
+
+    printf("------------------ Program Metadata ------------------\n");
+    printf("Main process created %d child processes to count words in %d files.\n", totalProcesses, totalFiles);
+    printf("%d files have been counted sucessfully.\n%d files did not exist.\n", totalSuccessfulFiles, totalUselessFiles);
  
     return 0;
 }
@@ -67,13 +110,10 @@ int processFile(char *filename){
         
     }
     
-    printf("%d total words.\n", totalWordCount);
-    
-    
     fclose(fp);
     
     
-    return 0;
+    return totalWordCount;
 }
 
 char* tokenizeString(char* string){
